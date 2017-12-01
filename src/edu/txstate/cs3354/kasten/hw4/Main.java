f6aa1f39c07d69917fb1103f0dc6ec0ee64a3377
Nick Kasten <kastencode@gmail.com>
2017-11-30 18:09:36 -0600

    cleaned up test code

--- src/edu/txstate/cs3354/kasten/hw4/Main.java
+++ src/edu/txstate/cs3354/kasten/hw4/Main.java
@@ -2,8 +2,8 @@ package edu.txstate.cs3354.kasten.hw4;
 
 public class Main {
 	public static void main (String[] args) {
-		// System.out.println("hello homework.");
-		Board myBoard = new Board(20);
+		// create new board with certain size
+		Board myBoard = new Board(10);
 		
 		// output board to the console
 		BoardDisplay.display(myBoard);
